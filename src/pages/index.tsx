06ae1cbd3adad0d91cc33cfb36b071fedf8ea56a
Shehzad-97 <shehzuali5@gmail.com>
2022-08-31 20:47:59 +0500

    setup next typescript with talwind css

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+const Home: NextPage = () => {
+  return (
+    <h1 className='bg-blue-500 text-center'>Wlcome to Next Js With Typescript</h1>
+  )
+}
+
+export default Home
