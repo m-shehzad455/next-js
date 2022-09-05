adb8f6e5ddb06bd166105119b5f23d671448000d
m-shehzad455 <shehzuali5@gmail.com>
2022-09-05 22:27:52 +0500

    navabr

--- src/Components/Common/Navbar/index.tsx
+++ src/Components/Common/Navbar/index.tsx
@@ -4,7 +4,8 @@ import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import {TbSettings} from "react-icons/tb"
 import {MdNotificationsNone} from "react-icons/md"
-import {FiMessageSquare,FiPackage} from "react-icons/fi"
+import {FiMessageSquare,FiPackage,FiUser} from "react-icons/fi"
+import {HiOutlineEnvelope,HiArrowDown} from "react-icons/hi"
 import { US,NZ, AQ,AT,PK,IN,CH,IR,SR,EG } from 'country-flag-icons/react/3x2'
 import Image from 'next/image'
 import  {useState}  from 'react'
@@ -22,47 +23,7 @@ function classNames(...classes:any) {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
             <div className="flex h-16 justify-between">
-              {/* <div className="flex px-2 lg:px-0">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                    alt="Workflow"
-                  />
-                </div>
-                <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
-                 
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900"
-                  >
-                    Dashboard
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500  hover:text-gray-700"
-                  >
-                    Team
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500  hover:text-gray-700"
-                  >
-                    Projects
-                  </a>
-                  <a
-                    href="#"
-                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500  hover:text-gray-700"
-                  >
-                    Calendar
-                  </a>
-                </div>
-              </div> */}
+              
               <div className="flex flex-1 items-center justify-center px-2 lg:ml-6 lg:justify-between">
                 <div className="w-full max-w-lg lg:max-w-xs">
                   <label htmlFor="search" className="sr-only">
@@ -87,17 +48,6 @@ function classNames(...classes:any) {
                 
                </div>
 
-              {/* <div className="flex items-center">
-             
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div> */}
               <div className="hidden lg:ml-4 lg:flex lg:items-center">
                 
                 
@@ -106,7 +56,7 @@ function classNames(...classes:any) {
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
                     
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-white text-sm ">
                       <span className="sr-only">Open user menu</span>
                       <NZ className="h-6 w-6"/>
 
@@ -146,16 +96,6 @@ function classNames(...classes:any) {
                         
                       </Menu.Item>
                       
-                      {/* <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item> */}
                       <Menu.Item>
                         {({ active }) => (
                           <a
@@ -176,7 +116,7 @@ function classNames(...classes:any) {
                 
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-white text-sm ">
                       <span className="sr-only">Open user menu</span>
                       < FiPackage className="h-6 w-6" aria-hidden="true" />
                     </Menu.Button>
@@ -226,9 +166,11 @@ function classNames(...classes:any) {
                 </Menu>
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-white text-sm ">
                       <span className="sr-only">Open user menu</span>
                       < FiMessageSquare className="h-6 w-6" aria-hidden="true" />
+                      <span className='h-4 w-4  flex text-center justify-center absolute text-white rounded-full bg-red-500  top-0 right-0'>2</span>
+
                     </Menu.Button>
                   </div>
                   <Transition
@@ -276,7 +218,7 @@ function classNames(...classes:any) {
                 </Menu>
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-white text-sm">
                       <span className="sr-only">Open user menu</span>
                        <MdNotificationsNone className="h-6 w-6" aria-hidden="true" />
                     </Menu.Button>
@@ -326,13 +268,14 @@ function classNames(...classes:any) {
                 </Menu>
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className=" relative flex rounded-full bg-white text-sm  focus:ring-indigo-500 focus:ring-offset-2">
                       <img
-                        className="h-8 w-8 rounded-full"
+                        className="h-8 w-8 rounded-full relative
+                        "
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
+                      <span className='h-4 w-4  flex text-center justify-center absolute text-white rounded-full bg-red-500  top-0 right-0'>2</span>
                         
                     </Menu.Button>
                   </div>
@@ -345,43 +288,70 @@ function classNames(...classes:any) {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className=" absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
+                         <a href="#"
+                         className={classNames(active ? 'bg-gray-100' : '', 'flex justify-between')}>
+                       <div className='flex justify-center text-center space-x-4'><FiPackage  className='h-6 w-6 '/>Inbox</div>
+                       <div className='h-6 w-6  flex text-center justify-center  text-white rounded-full bg-red-500 '>2</div>
+
+                       </a>
+                       
+                        
+                         
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
+                         <a href="#"
+                         className={classNames(active ? 'bg-gray-100' : '', 'flex justify-between')}>
+                       <div className='flex justify-center text-center'><FiPackage />Inbox</div>
+                       <div className='h-6 w-6  flex text-center justify-center  text-white rounded-full bg-red-500 '>3</div>
+
+                       </a>
+                       
+                          
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
+                          <a href="#"
+                          className={classNames(active ? 'bg-gray-100' : '', 'flex justify-between')}>
+                        <div className='flex justify-center text-center'><FiPackage />Inbox</div>
+                        
+                        </a>
+
+                          
                         )}
                       </Menu.Item>
+                      <Menu.Item>
+                        
+                        {({ active }) => (
+                          
+                          // <a
+                          //   href="#"
+                          //   className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 flex')}
+                          // >
+                          //   <FiPackage/>Inbox
+                          // </a>
+                          <a href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'flex justify-between')}>
+                          <div className='flex justify-center text-center'><FiPackage />Inbox</div>
+                          
+                          </a>
+                          
+                          
+                        )}
+                      </Menu.Item>
+                      
                     </Menu.Items>
+                    
                   </Transition>
                 </Menu>
                 <Menu as="div" className="relative ml-4 flex-shrink-0">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    <Menu.Button className="flex rounded-full bg-white text-sm ">
                       <span className="sr-only">Open user menu</span>
                       <TbSettings className='h-6 w-6' />
                     </Menu.Button>
@@ -482,7 +452,6 @@ function classNames(...classes:any) {
       </Dialog>
     </Transition.Root>
   
-
           </div>
 
           {/* <Disclosure.Panel className="">
